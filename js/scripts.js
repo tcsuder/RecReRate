e6596fe63ac81dc59dcd2d4ec28090e4c0a8838b
cc and ts <cc>
2016-02-01 14:55:44 -0800

    fixin stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-function Park(parkName, parkLocation) {
+function Park(parkName) {
   this.parkName = parkName;
   this.amenities = [];
   this.reviews = [];
+  this.allParks = [];
   this.north = false;
   this.northEast = false;
   this.southEast = false;
   this.northWest = false;
   this.southWest = false;
+}
+
+Park.prototype.parkCompiler = function(north, northEast, southEast, northWest, southWest) {
+  var peninsula = new Park("Peninsula Park");
+  peninsula.north = true;
+  var fernhill = new Park("Fernhill Park");
+  peninsula.northEast = true;
+  var allParks = [peninsula, fernhill];
+  var userParks = [];
+  for (var i = 0; i < allParks.length; i++) {
+  if (allParks[i].north === true) {
+    userParks.push(allParks[i]);
+    console.log(userParks);
+    }
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,40 @@
 describe('Park', function() {
   it("creates a Park object with the given specifications", function() {
     var testPark = new Park("Peninsula Park");
+    testPark.north = true;
     expect(testPark.parkName).to.equal("Peninsula Park");
     expect(testPark.amenities).to.eql([]);
     expect(testPark.reviews).to.eql([]);
-    expect(testPark.north).to.equal(false);
+    expect(testPark.allParks).to.eql([]);
+    expect(testPark.north).to.equal(true);
     expect(testPark.northEast).to.equal(false);
     expect(testPark.southEast).to.equal(false);
     expect(testPark.northWest).to.equal(false);
     expect(testPark.southWest).to.equal(false);
   });
+
+
+  it("will find all parks in one or more given locations", function() {
+    var peninsula = new Park("Peninsula Park");
+    peninsula.north = true;
+    var fernhill = new Park("Fernhill Park");
+    peninsula.northEast = true;
+    var allParks = [peninsula, fernhill];
+    expect(parkCompiler(true, true, false, false, false)).to.equal
+  });
 });
+
+// describe('Location', function() {
+//   it("creates a location object with the given specifications", function() {
+//     var testLocation = new Location("NE");
+//     testPark.north = true;
+//     expect(testPark.parkName).to.equal("Peninsula Park");
+//     expect(testPark.amenities).to.eql([]);
+//     expect(testPark.reviews).to.eql([]);
+//     expect(testPark.allParks).to.eql([]);
+//     expect(testPark.north).to.equal(true);
+//     expect(testPark.northEast).to.equal(false);
+//     expect(testPark.southEast).to.equal(false);
+//     expect(testPark.northWest).to.equal(false);
+//     expect(testPark.southWest).to.equal(false);
+//   });
